fd5888fe7bcf029c30600c3b66b77d6fc849e8b6
Julia Seidman <juliaseid@gmail.com>
2020-05-14 11:07:51 -0700

    Create dictionary and work through ideas about project structure

--- Scrabble/Models/Word.cs
+++ Scrabble/Models/Word.cs
@@ -4,13 +4,25 @@ namespace Scrabble.Models
 {
   public class Word
   {
-    public { get; set; }
-    public  { get; set; }
+    public Dictionary<string, int> WordValues { get; set; } = new Dictionary<string, int>() { {'a', 1}, {'e', 1}, {'i', 1}, {'o', 1}, {'u', 1}, {'l', 1}, {'n', 1}, {'r', 1}, {'s', 1}, {'t', 1}, {'g', 2}, {'b', 3}, {'c', 3}, {'m', 3}, {'p', 3}, {'f', 4}, {'h', 4}, {'v', 4}, {'w', 4}, {'y', 4}, {'k', 5}, {'j', 8}, {'x', 8}, {'q', 10}, {'z', 10}};
+    public string word {get; set;}
+  
 
     public Word ()
     {
-
-
-
+    }
   }
 }
+
+//public Word (dictionary of letter:points)
+//{
+  //Dict = dict
+  //_instances.Add(this)
+//}
+//public Word (dict, string word)
+//{
+  //:this(dict)
+  //{
+    //word=word;
+  //}
+//}
